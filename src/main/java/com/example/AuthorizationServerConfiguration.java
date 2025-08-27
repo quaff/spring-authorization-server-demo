b3700cd3eb4490879af8c86d6bb172bfac17ab33
Yanming Zhou <zhouyanming@gmail.com>
2025-08-27 15:28:17 +0800

    Extract AuthorizationServerConfiguration from SecurityConfiguration

---
+++ src/main/java/com/example/AuthorizationServerConfiguration.java
@@ -0,0 +1,120 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CompositeHttpSessionIdResolver;
+import org.springframework.session.web.http.CookieHttpSessionIdResolver;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Configuration
+@EnableRedisHttpSession
+class AuthorizationServerConfiguration {
+
+    @Bean
+    @Order(0)
+    SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                OAuth2AuthorizationServerConfigurer.authorizationServer();
+
+        http.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, Customizer.withDefaults())
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                // Redirect to the login page when not authenticated from the
+                // authorization endpoint
+                .exceptionHandling((exceptions) -> exceptions
+                        .defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                        )
+                )
+                .csrf(CsrfConfigurer::disable)
+                .cors(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public JdbcOAuth2AuthorizationService oauth2AuthorizationService(JdbcOperations jdbcOperations, RegisteredClientRepository registeredClientRepository,
+                                                                     SessionRepository<? extends Session> sessionRepository, Environment env) {
+        return new JdbcOAuth2AuthorizationService(jdbcOperations, registeredClientRepository) {
+            @Override
+            public OAuth2Authorization findByToken(String token, OAuth2TokenType tokenType) {
+                Session session = sessionRepository.findById(token);
+                if (session != null) {
+                    SecurityContext context = session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+                    if (context != null) {
+                        Authentication authentication = context.getAuthentication();
+                        String username = authentication.getName();
+                        String clientId = "mcp-client";
+                        Set<String> scopes = Set.of("mcp");
+                        RegisteredClient client = registeredClientRepository.findByClientId(clientId);
+                        if (client == null) {
+                            return null;
+                        }
+                        Instant issuedAt = session.getCreationTime();
+                        Instant expiresAt = Instant.now().plus(session.getMaxInactiveInterval());
+                        OAuth2AccessToken accessToken = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, token, issuedAt, expiresAt
+                                .plus(Duration.ofDays(30)), scopes);
+                        OAuth2Authorization.Builder builder = OAuth2Authorization.withRegisteredClient(client);
+                        builder.token(accessToken, metadata -> {
+                                    Map<String, Object> claims = Map.of(
+                                            "sub", username,
+                                            "aud", List.of(clientId),
+                                            "nbf", issuedAt,
+                                            "scope", String.join(",", scopes),
+                                            "iss", env.getProperty("spring.security.oauth2.authorizationserver.issuer", ""),
+                                            "jti", token
+                                    );
+                                    metadata.put(OAuth2Authorization.Token.CLAIMS_METADATA_NAME, claims);
+                                }).authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                                .id(token).principalName(username).authorizedScopes(scopes);
+                        return builder.build();
+                    }
+                }
+                return super.findByToken(token, tokenType);
+            }
+        };
+    }
+
+    @Bean
+    public JdbcOAuth2AuthorizationConsentService oauth2AuthorizationConsentService(JdbcOperations jdbcOperations, RegisteredClientRepository registeredClientRepository) {
+        return new JdbcOAuth2AuthorizationConsentService(jdbcOperations, registeredClientRepository);
+    }
+
+    @Bean
+    HttpSessionIdResolver httpSessionIdResolver() {
+        return new CompositeHttpSessionIdResolver(HeaderHttpSessionIdResolver.xAuthToken(),
+                new CookieHttpSessionIdResolver());
+    }
+}
